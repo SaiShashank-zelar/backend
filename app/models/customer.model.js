3b589178ac2b04ef020472bc6021cef0d9af1309
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-11 21:57:10 +0530

    update

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -21,13 +21,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       defaultValue: "https://loizenai.com"
     }
-	},
-	{
-        // disable the modification of table names; By default, sequelize will automatically
-        // transform all passed model names (first parameter of define) into plural.
-        // if you don't want that, set the following
-        freezeTableName: true,
-    });
+	});
 	
 	return Customer;
 }

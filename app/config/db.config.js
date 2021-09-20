26c06f385597840e908cec1242644e0a548d94e5
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-20 16:24:59 +0530

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
  
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
-  host: process.env.host,
+  host: 'postgres',
   dialect: 'postgres',
   operatorsAliases: false,
  

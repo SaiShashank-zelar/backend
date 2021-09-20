cb89c956e0c3d0b45e91859153d092060396ac43
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-20 11:45:17 +0530

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,16 @@
-const env = require('./env.js');
+//const env = require('./env.js');
  
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
   operatorsAliases: false,
  
   pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 const db = {};

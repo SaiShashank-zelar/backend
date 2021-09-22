5c691a222e7a624c91d25dead8d5e7eb75919d58
SaiShashank-zelar <shashank@zelarsoft.com>
2021-09-22 16:15:57 +0530

    update

--- app/routers/router.js
+++ app/routers/router.js
@@ -3,10 +3,10 @@ let router = express.Router();
  
 const customers = require('../controllers/controller.js');
 
-router.post('/api/customers', customers.createCustomer);
-router.get('/api/customers/:id', customers.getCustomer);
+router.post('/api/customer', customers.createCustomer);
+router.get('/api/customer/:id', customers.getCustomer);
 router.get('/api/customers', customers.customers);
-router.put('/api/customers', customers.updateCustomer);
-router.delete('/api/customers/:id', customers.deleteCustomer);
+router.put('/api/customer', customers.updateCustomer);
+router.delete('/api/customer/:id', customers.deleteCustomer);
 
 module.exports = router;
